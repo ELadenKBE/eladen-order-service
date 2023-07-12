804100aecfc5bf1d67af777625615279ce77b72c
BorissowT <timsregisters@gmail.com>
2023-07-12 11:05:24 +0200

    implement user identification

--- order_service_v2/schema.py
+++ order_service_v2/schema.py
@@ -2,7 +2,6 @@ import graphene
 import graphql_jwt
 
 import orders.schema
-import users.schema
 
 
 class Query(orders.schema.Query):
@@ -10,11 +9,8 @@ class Query(orders.schema.Query):
 
 
 class Mutation(orders.schema.Mutation,
-               users.schema.Mutation,
                graphene.ObjectType,):
-    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
-    verify_token = graphql_jwt.Verify.Field()
-    refresh_token = graphql_jwt.Refresh.Field()
+    pass
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
