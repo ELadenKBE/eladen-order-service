f214c185f24229188985b7487095c02262970308
tim <timsregisters@gmail.com>
2023-07-22 11:41:26 +0200

    add connection error handling + banking and delivery queues

--- order_service_v2/checkout_producer.py
+++ order_service_v2/checkout_producer.py
@@ -1,6 +1,7 @@
 import json
 
 import pika as pika
+from pika.exceptions import StreamLostError
 
 from orders.models import Order
 from decouple import config
@@ -10,14 +11,28 @@ host = config('RABBITMQ_HOST', default="localhost", cast=str)
 
 
 class CheckoutProducer:
-    connection_params = pika.ConnectionParameters(host)
-    connection = pika.BlockingConnection(connection_params)
-    channel = connection.channel()
-    channel.queue_declare(queue='checkout_queue')
+    connection_params = None
+    connection = None
+    channel = None
+
+    def __init__(self):
+        self.connect()
+
+    def connect(self):
+        connection_params = pika.ConnectionParameters(host)
+        self.connection = pika.BlockingConnection(connection_params)
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue='checkout_queue')
 
     def publish_order(self, order: Order):
         message = json.dumps(model_to_dict(order))
-        self.channel.basic_publish(exchange='',
-                                   routing_key='checkout_queue',
-                                   body=message)
-        self.connection.close()
+        try:
+            self.channel.basic_publish(exchange='',
+                                       routing_key='checkout_queue',
+                                       body=message)
+        except StreamLostError as e:
+            self.connect()
+            self.publish_order(order)
+        except Exception as e:
+            pass
+
